ed2fb715d72b5f927db31c28a88caa5cedc0c583
Christian Peters <cpeters@Christians-MacBook-Air.local>
2025-08-25 21:30:51 -0400

    init commit, init tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="text-center text-2xl font-bold text-blue-500">
+      <h1 className="mt-4">Welcome to GeoGuessHer, a project by Christian Peters</h1>
+    </div>
+  );
+}
+
+export default App;
