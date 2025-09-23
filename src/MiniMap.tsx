a4488dca5e49d99b1f5dd1f30f1fffc3c7c6f78b
Christian Peters <christianpeters31@outlook.com>
2025-09-22 21:15:59 -0400

    the map works!!!!

--- src/MiniMap.tsx
+++ src/MiniMap.tsx
@@ -1,7 +1,15 @@
+import { Map } from "@vis.gl/react-google-maps";
+
 export default function MiniMap() {
   return (
-    <div className="absolute bottom-4 right-4 w-64 h-48 bg-gray-200/90 border-2 border-white rounded-lg z-50 flex items-center justify-center shadow-lg">
-      <span className="text-sm text-gray-800">MiniMap placeholder</span>
+    <div className="absolute bottom-4 right-4 w-64 h-48 border-2 border-white rounded-lg overflow-hidden shadow-lg z-50">
+      <Map
+        defaultZoom={2}
+        defaultCenter={{ lat: 20, lng: 0 }} // Roughly world center
+        gestureHandling="greedy"
+        disableDefaultUI={true}
+        style={{ width: "100%", height: "100%" }}
+      />
     </div>
   );
 }
