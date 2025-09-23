7761052342ac518c273224a9cd4137432b9df626
Christian Peters <christianpeters31@outlook.com>
2025-09-22 21:13:48 -0400

    chore: updated styles and css now works woooo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import StreetView from "./StreetView";
+import MiniMap from "./MiniMap";
 
 export default function App() {
-  return <StreetView lat={42.345573} lng={-71.098326} />;
+  return (
+    <div className="relative w-screen h-screen">
+      <StreetView lat={42.345573} lng={-71.098326} />
+      <MiniMap />
+    </div>
+  );
 }
---
+++ src/MiniMap.tsx
@@ -0,0 +1,7 @@
+export default function MiniMap() {
+  return (
+    <div className="absolute bottom-4 right-4 w-64 h-48 bg-gray-200/90 border-2 border-white rounded-lg z-50 flex items-center justify-center shadow-lg">
+      <span className="text-sm text-gray-800">MiniMap placeholder</span>
+    </div>
+  );
+}
