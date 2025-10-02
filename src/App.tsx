140bdb0821f4306b0f715b3f8ea35493623eb03b
Christian Peters <christianpeters31@outlook.com>
2025-10-01 20:52:33 -0400

    chore: button in separate file

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import StreetView from "./StreetView";
 import MiniMap from "./MiniMap";
 import { useDistanceCalculation } from "./useDistanceCalculation";
+import SubmitButton from "./SubmitButton";
+
+export interface MapCoordinates {
+  lat: number;
+  lng: number;
+}
 
 export default function App() {
-  const [guess, setGuess] = useState<{ lat: number; lng: number } | null>(null);
+  const [guess, setGuess] = useState<MapCoordinates | null>(null);
 
-  const location = { lat: 42.345573, lng: -71.098326 };
+  const location: MapCoordinates = { lat: 42.345573, lng: -71.098326 };
 
   const { getDistance } = useDistanceCalculation();
 
@@ -22,20 +28,7 @@ export default function App() {
     <div className="relative w-screen h-screen">
       <StreetView lat={location.lat} lng={location.lng} />
       <MiniMap guess={guess} setGuess={setGuess} />
-
-      <div className="absolute bottom-4 left-4 z-50">
-        <button
-          onClick={handleSubmit}
-          disabled={!guess}
-          className={`px-4 py-2 rounded shadow text-white ${
-            guess
-              ? "bg-blue-600 hover:bg-blue-700"
-              : "bg-gray-400 cursor-not-allowed"
-          }`}
-        >
-          Submit Guess
-        </button>
-      </div>
+      <SubmitButton handleSubmit={handleSubmit} guess={guess}></SubmitButton>
     </div>
   );
 }
---
+++ src/SubmitButton.tsx
@@ -0,0 +1,27 @@
+import type { MapCoordinates } from "./App";
+
+type SubmitButtonProps = {
+  handleSubmit: () => void;
+  guess: MapCoordinates | null;
+};
+
+export default function SubmitButton({
+  handleSubmit,
+  guess,
+}: SubmitButtonProps) {
+  return (
+    <div className="absolute bottom-4 left-4 z-50">
+      <button
+        onClick={handleSubmit}
+        disabled={!guess}
+        className={`px-4 py-2 rounded shadow text-white ${
+          guess
+            ? "bg-blue-600 hover:bg-blue-700"
+            : "bg-gray-400 cursor-not-allowed"
+        }`}
+      >
+        Submit Guess
+      </button>
+    </div>
+  );
+}
